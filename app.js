2f6cba42161ff74242257c2e9bfaaf280278783b
Z杭 <Z@DESKTOP-DDE0QN1>
2017-05-12 09:04:10 +0800

    5

--- app.js
+++ app.js
@@ -138,38 +138,40 @@ app.post('/delete',function(req,res){
 
 //cheshi
 app.get('/',function(req,res){
-	var insertData = function(db, callback) {  
-	    //连接到表 sites
-	    var collection = db.collection('sitesUser');
-	    //插入数据
-	 var data = [{"rating":{"max":10,"numRaters":1,"average":"0.0","min":0},"subtitle":"","author":["理查兹"],"pubdate":"2007-3","tags":[{"count":1,"name":"英语","title":"英语"},{"count":1,"name":"藏书","title":"藏书"},{"count":1,"name":"语言·写作","title":"语言·写作"}],"origin_title":"","image":"https://img1.doubanio.com\/mpic\/s5811478.jpg","binding":"","translator":[],"catalog":"","pages":"113","images":{"small":"https://img1.doubanio.com\/spic\/s5811478.jpg","large":"https://img1.doubanio.com\/lpic\/s5811478.jpg","medium":"https://img1.doubanio.com\/mpic\/s5811478.jpg"},"alt":"https:\/\/book.douban.com\/subject\/2056914\/","id":"2056914","publisher":"外语教学与研究","isbn10":"7560062415","isbn13":"9787560062419","title":"剑桥国际英语教程","url":"https:\/\/api.douban.com\/v2\/book\/2056914","alt_title":"","author_intro":"","summary":"《剑桥国际英语教程》（第3版）这套教材的主要产品包括学生用书（附赠词汇手册）、教师用书。练习册、录音带或CD、录像教材、DVD和CD-ROM等。另外，学生用书和练习册分两个版本——全一册和A、B分册，便于广大师生根据需要选择。录像教材可以作为视听说培训教材单独使用。主要特色：综合培养听说读写技能，兼顾准备和流利度，在交际语境中学习语法，在任务型活动中训练听力，富有时代气息的话题，生动自然的对话语言，全新的语音学习大纲，活泼有趣的口语活动，完善的复习和测试系统，独特的单元自学听力练习，寓教于乐的视听说配套产品，科学的教师培训服务体系。\n\n 剑桥国际英语教程","price":"49.90元"}];
-	    collection.insert(data, function(err, result) { 
-	        if(err)
-	        {
-	            console.log('Error:'+ err);
-	            return;
-	        }else{
-	        	console.log('存取成功')
-	        }
-	        callback(result);
-	    });
+	var password = req.body.password;
+	//首先得从库里拿到数据
+	var selectData=function(db,callback){
+		//连接到数据文档
+		var collection=db.collection('sites');
+		//查询数据
+		var whereStr={"title":"HTML5 App商业开发实战教程"};  //我们要查询的信息是所有包含这个内容的数据。
+		collection.find(whereStr).toArray(function(err,result){
+			if(err){
+				console.log('Error:'+err);
+				return;
+			}
+			callback(result);
+		})
 	}
-
-	MongoClient.connect(DB_CONN_STR, function(err, db) {
-	    console.log("连接成功！");
-	    insertData(db, function(result) {
-	        console.log(result);
-	        db.close();
-	    });
-	});
-	var response ='<html><head><title>Simple Send</title></head>'+
-				  '<body><h1>Hello form Express</h1></body></html>';
-	res.status(200);
-	res.set({
-		'Content-Type':'text/html',
-		'Content-Length':response.length
-	});
-	res.send(response);
+	
+	MongoClient.connect(DB_CONN_STR,function(err,db){
+		console.log("连接成功");
+		selectData(db,function(result){
+			console.log(result);
+			//把数据返回给前端
+			res.status(200),
+			res.json(result)	
+			db.close();
+		})
+	})
+//	var response ='<html><head><title>Simple Send</title></head>'+
+//				  '<body><h1>Hello form Express</h1></body></html>';
+//	res.status(200);
+//	res.set({
+//		'Content-Type':'text/html',
+//		'Content-Length':response.length
+//	});
+//	res.send(response);
 })
 
 //添加用户信息

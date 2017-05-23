691f5db7f54b852ab6caf9f8868feb8e6216d05b
Z杭 <Z@DESKTOP-DDE0QN1>
2017-05-23 09:54:20 +0800

    sa

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var DB_CONN_STR = 'mongodb://localhost:27017/book';
+
 //配置node服务器相关内容：
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParder = require('body-parser');
+//引入jade模块
+app.set('views','./views/pages')
+app.set('view engine','jade')//模块引擎
+
+//配置https链接
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
@@ -14,6 +21,7 @@ var certificate = fs.readFileSync('https/certificate.pem', 'utf8');  
 app.use(bodyParder.urlencoded({extended:true}));
 var options={key:privatekey, cert:certificate}; 
 var server = https.createServer(options, app);
+
 
 //设置跨域访问
 app.all('*', function(req, res, next) {
@@ -25,24 +33,50 @@ app.all('*', function(req, res, next) {
    next();
 });
 
+//cheshi
+app.get('/',function(req,res){
+//	response.writeHead(200, {"Content-Type": "text/html"});
+//	response.write(html);
+//	response.end();
+	var response ='<html><head><title>Simple Send</title></head>'+
+				  '<body><h1>Hello form Express</h1></body></html>';
+	res.status(200);
+	res.set({
+		'Content-Type':'text/html',
+		'Content-Length':response.length
+	});
+	res.send(response);
+})
+
+
 //添加数据
-app.post('/add',function(req,res){
+app.post('/add',function(req,res)  {
 var insertData = function(db, callback) {
     //连接到表 sites
     var collection = db.collection('sites');
     //插入数据
+    var author = "理查兹"; //作者(数组)
+    var pudate = "2007-3"; //上架日期
+    var bookImage = "https://img1.doubanio.com\/mpic\/s5811478.jpg"; //图片
+    var id = "2056914"; //图书id
+    var publisher = "外语教学与研究"; //出版社
+    var isbn10 = "7560062415"; //isbn编码
+    var isbn13 = "9787560062419";
+    var title = "剑桥国际英语教程"; //名称
+    //简介
+    var summary = "《剑桥国际英语教程》（第3版）这套教材的主要产品包括学生用书（附赠词汇手册）、教师用书。练习册、录音带或CD、录像教材、DVD和CD-ROM等。另外，学生用书和练习册分两个版本——全一册和A、B分册，便于广大师生根据需要选择。录像教材可以作为视听说培训教材单独使用。主要特色：综合培养听说读写技能，兼顾准备和流利度，在交际语境中学习语法，在任务型活动中训练听力，富有时代气息的话题，生动自然的对话语言，全新的语音学习大纲，活泼有趣的口语活动，完善的复习和测试系统，独特的单元自学听力练习，寓教于乐的视听说配套产品，科学的教师培训服务体系。\n\n 剑桥国际英语教程"; 	
+ 	var price = "49.90元";//价格 	
+ 	var page = "113";//页数
+ 	//藏书量
+ 	//分类
  var data = [{
-	        "rating": {
-	            "max": 10,
-	            "numRaters": 1,
-	            "average": "0.0",
-	            "min": 0
-	        },
+	        //副标题
 	        "subtitle": "",
-	        "author": [
-	            "理查兹"
-	        ],
-	        "pubdate": "2007-3",
+	        //作者
+	        "author": author,
+	        //上架日期
+	        "pubdate": pudate,
+	        //分类
 	        "tags": [
 	            {
 	                "count": 1,
@@ -60,31 +94,50 @@ var insertData = function(db, callback) {
 	                "title": "语言·写作"
 	            }
 	        ],
+	        //组织名称
 	        "origin_title": "",
-	        "image": "https://img1.doubanio.com\/mpic\/s5811478.jpg",
+	        //图片
+	        "image": bookImage,
+	        //装订
 	        "binding": "",
+	        //翻译者
 	        "translator": [
 	            
 	        ],
+	        //目录
 	        "catalog": "",
-	        "pages": "113",
+	        //页数
+	        "pages": page,
 	        "images": {
 	            "small": "https://img1.doubanio.com\/spic\/s5811478.jpg",
 	            "large": "https://img1.doubanio.com\/lpic\/s5811478.jpg",
 	            "medium": "https://img1.doubanio.com\/mpic\/s5811478.jpg"
 	        },
 	        "alt": "https:\/\/book.douban.com\/subject\/2056914\/",
-	        "id": "2056914",
+	        //图书id
+	        "id": id,
+	        //出版社
 	        "publisher": "外语教学与研究",
-	        "isbn10": "7560062415",
-	        "isbn13": "9787560062419",
-	        "title": "剑桥国际英语教程",
+	        //isbn编码
+	        "isbn10": isbn10,
+	        "isbn13": isbn13,
+	        //名称
+	        "title": title,
+	       	//地址
 	        "url": "https:\/\/api.douban.com\/v2\/book\/2056914",
 	        "alt_title": "",
+	        //作者简介
 	        "author_intro": "",
-	        "summary": "《剑桥国际英语教程》（第3版）这套教材的主要产品包括学生用书（附赠词汇手册）、教师用书。练习册、录音带或CD、录像教材、DVD和CD-ROM等。另外，学生用书和练习册分两个版本——全一册和A、B分册，便于广大师生根据需要选择。录像教材可以作为视听说培训教材单独使用。主要特色：综合培养听说读写技能，兼顾准备和流利度，在交际语境中学习语法，在任务型活动中训练听力，富有时代气息的话题，生动自然的对话语言，全新的语音学习大纲，活泼有趣的口语活动，完善的复习和测试系统，独特的单元自学听力练习，寓教于乐的视听说配套产品，科学的教师培训服务体系。\n\n 剑桥国际英语教程",
-	        "price": "49.90元"
+	       	//摘要信息
+	        "summary": summary,
+	        //价格
+	        "price": price,
+	        //评论
+	        "comments":{
+	        	
+	        }
 	    }];
+	    console.log('这是模块' + data);
 	 collection.insert(data, function(err, result) { 
         if(err)
         {
@@ -107,7 +160,7 @@ MongoClient.connect(DB_CONN_STR, function(err, db) {
 })
 
 //查询数据
-app.post('/find',function(req,res){
+app.get('/find',function(req,res){
 	var password = req.body.password;
 	//首先得从库里拿到数据
 	var selectData=function(db,callback){
@@ -195,47 +248,11 @@ app.post('/delete',function(req,res){
 	});
 })
 
-//cheshi
-app.get('/',function(req,res){
-	var password = req.body.password;
-	//首先得从库里拿到数据
-	var selectData=function(db,callback){
-		//连接到数据文档
-		var collection=db.collection('sites');
-		//查询数据
-		var whereStr={"title":"HTML5 App商业开发实战教程"};  //我们要查询的信息是所有包含这个内容的数据。
-		collection.find(whereStr).toArray(function(err,result){
-			if(err){
-				console.log('Error:'+err);
-				return;
-			}
-			callback(result);
-		})
-	}
-	
-	MongoClient.connect(DB_CONN_STR,function(err,db){
-		console.log("连接成功");
-		selectData(db,function(result){
-			console.log(result);
-			//把数据返回给前端
-			res.status(200),
-			res.json(result)	
-			db.close();
-		})
-	})
-//	var response ='<html><head><title>Simple Send</title></head>'+
-//				  '<body><h1>Hello form Express</h1></body></html>';
-//	res.status(200);
-//	res.set({
-//		'Content-Type':'text/html',
-//		'Content-Length':response.length
-//	});
-//	res.send(response);
-})
+
 
 //添加用户信息
 app.post('/addUser',function(req,res){
-var insertData = function(db, callback) {  
+var insertData = function(db, callback) {
     //连接到表 sites
     var collection = db.collection('sitesUser');
     //插入数据

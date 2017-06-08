77cf2954af128b46975823ab66e7d9cd17fdf05f
Z杭 <Z@DESKTOP-DDE0QN1>
2017-06-08 09:36:03 +0800

    1

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.all('*', function(req, res, next) {  
 	next();
 });
 //cheshi
-app.get("/",function(req,res){
+app.post("/",function(req,res){
 	var isbn = req.body.isbn;
 	console.log(isbn)
 	res.status(200),
